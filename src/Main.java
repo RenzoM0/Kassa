ca72cbe65f455937b273c1376bbda56aecb8f267
Renzo M <lma.macnack@outlook.com>
2018-10-05 10:48:35 +0200

    Aan Gui kloten

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,9 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //GUI zichtbaar maken
+        KassaForm form = new KassaForm();
+        form.setVisible(true);
     }
-    
+
 }
